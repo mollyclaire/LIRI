9cb225d4a272e4b4e08c4f3ed578c0eb1ef261d5
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-23 11:39:09 -0500

    Trying to get spotify-this-song working.

--- liri.js
+++ liri.js
@@ -14,7 +14,8 @@ var command = process.argv[2];
 
 // Variable for user data inputs
 var userInput = process.argv[3];
-if (command === "concert-this") {
+
+
 // Bands in Town function
 function concert(userInput) {
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
@@ -23,7 +24,7 @@ function concert(userInput) {
             console.log("Venue name: " + response.data[0].venue.name);
             console.log("Venue location: " + response.data[0].venue.city);
             console.log("Date: " + date);
-            console.log("===================================================================================");
+            console.log("=================================================================================");
         }
     ) .catch(function(error) {
         if (error.response) {
@@ -45,18 +46,59 @@ function concert(userInput) {
         }
         console.log(error.config);
       });
-};} concert(userInput);
+}; concert(userInput);
+
 
 // Spotify function
 function spotifySearch(userInput) {
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
+    if (!userInput) {
+        userInput = "The Sign";
+        spotifySearch(userInput);
+    } else { 
+        spotify.search({ 
+            type: "track", 
+            query: userInput,
+            limit: 3
+        }) 
+        .then(function(response) {
+            let songObject = response.tracks.items;
+            console.log(response);
+            for (i = 0; i < songObject.length; i ++) {
+            console.log("Artist: " + songObject.songs.artists[i].name);
+            console.log("Song name: " + songObject.songs[i].name);
+            console.log("Preview: " + songObject.songs[i].preview_url);
+            console.log("Album name: " + songObject.songs[i].album.name);
+            console.log("=================================================================================")};
+    }) .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+    
+}}; spotifySearch(userInput);
+
+function liri() {
+    switch(userInput) {
+        case "concert this":
+            concert()
+            break;
+        case "spotify-this-song":
+            spotifySearch(userInput)
+            break;
     }
-   
-  console.log(data); 
-  });
-}
-// OMDB
-
-
+} liri(); 
