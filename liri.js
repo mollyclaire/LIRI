f1252ae36a85e74053f38927d38beec9bcbec94b
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-23 10:37:14 -0500

    concert-this command functioning.

--- liri.js
+++ liri.js
@@ -1,4 +1,62 @@
+// Requires the data in my .env file 
 require("dotenv").config();
+
+// Declares variables for dependencies 
 var keys = require("./keys.js");
+Spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var moment = require("moment");
+var fs = require("fs");
+var axios = require("axios");
+
+// Variable for user commands ("concert-this" "spotify-this-song" "movie-this" "do-what-it-says")
 var command = process.argv[2];
+
+// Variable for user data inputs
+var userInput = process.argv[3];
+if (command === "concert-this") {
+// Bands in Town function
+function concert(userInput) {
+    axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            var date = moment(response.data[0].datetime).format('MM/DD/YYYY');
+            console.log("Venue name: " + response.data[0].venue.name);
+            console.log("Venue location: " + response.data[0].venue.city);
+            console.log("Date: " + date);
+            console.log("===================================================================================");
+        }
+    ) .catch(function(error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log("---------------Data---------------");
+          console.log(error.response.data);
+          console.log("---------------Status---------------");
+          console.log(error.response.status);
+          console.log("---------------Status---------------");
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an object that comes back with details pertaining to the error that occurred.
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+        }
+        console.log(error.config);
+      });
+};} concert(userInput);
+
+// Spotify function
+function spotifySearch(userInput) {
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+   
+  console.log(data); 
+  });
+}
+// OMDB
+
+
