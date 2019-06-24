b486a0c246c69fe9b7fe90038a84ecb4c5009d7c
Molly Graham <mollyclaire.graham@gmail.com>
2019-06-24 18:28:09 -0500

    spotify-this-song function working. Added chalk styling to terminal.

--- liri.js
+++ liri.js
@@ -8,12 +8,13 @@ var spotify = new Spotify(keys.spotify);
 var moment = require("moment");
 var fs = require("fs");
 var axios = require("axios");
+var chalk = require("chalk");
 
 // Variable for user commands ("concert-this" "spotify-this-song" "movie-this" "do-what-it-says")
 var command = process.argv[2];
 
 // Variable for user data inputs
-var userInput = process.argv[3];
+var userInput = process.argv.splice(3).join(" ");
 
 
 // Bands in Town function
@@ -22,10 +23,10 @@ function concert(userInput) {
     axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
         function (response) {
             var date = moment(response.data[0].datetime).format('MM/DD/YYYY');
-            console.log("Venue name: " + response.data[0].venue.name);
-            console.log("Venue location: " + response.data[0].venue.city);
-            console.log("Date: " + date);
-            console.log("=================================================================================");
+            console.log(chalk.bold("Venue name: ") + response.data[0].venue.name);
+            console.log(chalk.bold("Venue location: ") + response.data[0].venue.city);
+            console.log(chalk.bold("Date: ") + date);
+            console.log(chalk.blue(("=============================================")));
         }
     )
     // Is there is an error
@@ -51,25 +52,25 @@ function concert(userInput) {
 function spotifySearch(userInput) {
     // If the user inputs nothing, use the song, "The Sign"
     if (!userInput) {
-        userInput = "The Sign";
+        userInput = "The Sign by Ace of Base";
         spotifySearch(userInput);
     } 
-    // Or search whatever track they input and limit the search to 3 results
+    // Or search whatever track they input and limit the search to 1 result
     else { 
         spotify.search({ 
             type: "track", 
             query: userInput,
-            limit: 3
+            limit: 1
         }) 
         .then(function(response) {
             let songObject = response.tracks.items;
-            console.log(response);
+            // console.log(response);
             for (i = 0; i < songObject.length; i ++) {
-            console.log("Artist: " + songObject[i].songs.artists.name);
-            console.log("Song name: " + songObject[i].songs.name);
-            console.log("Preview: " + songObject[i].songs.preview_url);
-            console.log("Album name: " + songObject[i].songs.album.name);
-            console.log("=================================================================================")};
+            console.log(chalk.bold("Artist: ") + songObject[i].artists[i].name);
+            console.log(chalk.bold("Song name: ") + songObject[i].name);
+            console.log(chalk.bold("Preview: ") + songObject[i].preview_url);
+            console.log(chalk.bold("Album name: ") + songObject[i].album.name);
+            console.log(chalk.blue(("=============================================")))};
     })
     // If there is an error
     .catch(function(error) {
@@ -102,15 +103,15 @@ function movie(userInput) {
     else {
         axios.get("http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy").then(
             function (response) {
-                console.log("Title: " + response.data.Title);
-                console.log("Year: " + response.data.Year);
-                console.log("IMDB rating: " + response.data.imdbRating);
-                console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-                console.log("Country: " + response.data.Country);
-                console.log("Language: " + response.data.Language);
-                console.log("Plot: " + response.data.Plot);
-                console.log("Actors: " + response.data.Actors);
-                console.log("=================================================================================");
+                console.log(chalk.bold("Title: ") + response.data.Title);
+                console.log(chalk.bold("Year: ") + response.data.Year);
+                console.log(chalk.bold("IMDB rating: ") + response.data.imdbRating);
+                console.log(chalk.bold("Rotten Tomatoes: ") + response.data.Ratings[1].Value);
+                console.log(chalk.bold("Country: ") + response.data.Country);
+                console.log(chalk.bold("Language: ") + response.data.Language);
+                console.log(chalk.bold("Plot: ") + response.data.Plot);
+                console.log(chalk.bold("Actors: ") + response.data.Actors);
+                console.log(chalk.blue(("=============================================")));
             }
         )
         // If there is an error
@@ -141,10 +142,10 @@ function doWhatItSays() {
         }
         console.log(content);
 
-        // ??
+        // Makes the content an array and divides it at the comma
         var array = content.split(",");
 
-        // ??
+        // The first item in the array is equal to the command. The second item is equal to userInput.
         var command = array[0];
         let userInput = array[1];
 
@@ -161,7 +162,7 @@ function doWhatItSays() {
     });
 };
 
-// Switch/cas function to make the program work
+// Switch/case function to make the program work
 function liri() {
     switch(command) {
         case "concert-this":
@@ -179,6 +180,8 @@ function liri() {
     }
 } liri(); 
 
+
+
 // function liri() {
 //     if (command == "concert-this") {
 //         concert(userInput);
@@ -190,3 +193,12 @@ function liri() {
 //         doWhatItSays();
 //     } else (console.log("Give a command"))
 // } liri();
+
+// function defaultSong() {
+//     spotify.search({ 
+//         type: "track", 
+//         query: "0hrBpAOgrt8RXigk83LLNE"
+//     }).then(function(response) {
+//         console.log(response);
+//     })
+// }
